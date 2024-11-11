bf5541b5e5d403d0e291d6c185a6a6b359bca85f
David Gallmeier <dg@xounges.net>
2024-11-10 23:35:45 -0500

    revise layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,7 +12,7 @@
 XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
 XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-E01  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  E02 \
+K38  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  K39 \
                XXX  K32  K33  K34       K35  K36  K37  XXX
 
 #define E01 &kp LSFT
