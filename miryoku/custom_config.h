71513c9384d911bb37d886e1f0bb74e9e7009b17
David Gallmeier <dg@xounges.net>
2024-11-10 23:51:28 -0500

    revise layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,28 +1,28 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-#if !defined (MIRYOKU_LAYOUTMAPPING_SOFLE)
-
-#define XXX &none
-
-#define MIRYOKU_LAYERMAPPING_BASE( \
-     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
-) \
-XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
-XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
-               XXX  K32  K33  K34       K35  K36  K37  XXX
-
-//#define K38 &kp LSFT
-//#define K39 &kp RSFT
-
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 53 54
-#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 55 56
-
-#endif
-
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_SOFLE
+//// Copyright 2021 Manna Harbour
+//// https://github.com/manna-harbour/miryoku
+//
+//#if !defined (MIRYOKU_LAYOUTMAPPING_SOFLE)
+//
+//#define XXX &none
+//
+//#define MIRYOKU_LAYERMAPPING_BASE( \
+//     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+//     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+//     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+//     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+//) \
+//XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
+//XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
+//XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
+//XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
+//               XXX  K32  K33  K34       K35  K36  K37  XXX
+//
+////#define K38 &kp LSFT
+////#define K39 &kp RSFT
+//
+//#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 53 54
+//#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 55 56
+//
+//#endif
+//
+//#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_SOFLE
